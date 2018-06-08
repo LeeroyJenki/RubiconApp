1c4c5638e7ff971911af4f52bf51c935a3bbd9ef
Unknown <mixail_956@rambler.ru>
2018-06-08 20:11:53 +0300

    добавил редактирование Note
    надо подправить массивы в NoteModel

--- app/src/main/java/ru/rubiconepro/study/Modules/NoteBook/Layout/NotesAdd.java
+++ app/src/main/java/ru/rubiconepro/study/Modules/NoteBook/Layout/NotesAdd.java
@@ -9,14 +9,19 @@ import android.widget.EditText;
 
 import java.util.ArrayList;
 
+import ru.rubiconepro.study.Modules.NoteBook.Adapter.PartAdapter;
 import ru.rubiconepro.study.Modules.NoteBook.Model.NotesModel;
+import ru.rubiconepro.study.Modules.NoteBook.Model.PartModel;
+import ru.rubiconepro.study.Modules.NoteBook.NoteBook;
 import ru.rubiconepro.study.R;
 
 public class NotesAdd extends AppCompatActivity implements View.OnClickListener {
     EditText titleNameN;
     EditText notesTextN;
     ArrayList<NotesModel> items;
-
+    int position;
+    int positionPart;
+    PartModel model;
 
 
     @Override
@@ -33,6 +38,14 @@ public class NotesAdd extends AppCompatActivity implements View.OnClickListener 
 
         items = new ArrayList<>();
 
+        Intent intt = getIntent();
+        positionPart = intt.getIntExtra("positionPart", 0);
+        position =  intt.getIntExtra("position", 0);
+        model = NoteBook.instance.getPartByPosition(positionPart);
+
+        titleNameN.setText(model.listNotes.get(position).title.toString());
+        notesTextN.setText(model.listNotes.get(position).text.toString());
+
     }
 
     @Override
@@ -42,6 +55,11 @@ public class NotesAdd extends AppCompatActivity implements View.OnClickListener 
         nt.title = titleNameN.getText().toString();
         nt.text = notesTextN.getText().toString();
         nt.notesList.add(nt);
+
+        model.listNotes.get(position).title = titleNameN.getText().toString();
+        model.listNotes.get(position).text = notesTextN.getText().toString();
+    //  model.listNotes.get(position).notesList.set(position, nt);
+
 
 
         Intent i = new Intent();

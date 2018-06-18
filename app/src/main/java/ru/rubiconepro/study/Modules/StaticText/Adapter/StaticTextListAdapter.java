e31909fe173b9254518ad947c922aad1e92c56b6
Unknown <genadij.kozlovich@bk.ru>
2018-06-18 20:58:11 +0300

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/ru/rubiconepro/study/Modules/StaticText/Adapter/StaticTextListAdapter.java
+++ app/src/main/java/ru/rubiconepro/study/Modules/StaticText/Adapter/StaticTextListAdapter.java
@@ -1,10 +1,38 @@
 package ru.rubiconepro.study.Modules.StaticText.Adapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+
+import java.util.List;
+import ru.rubiconepro.study.R;
+
+
+import ru.rubiconepro.study.Modules.Base.Interface.IPromptDialog;
+
+import ru.rubiconepro.study.Modules.StaticText.Model.StaticTextModel;
 
 public class StaticTextListAdapter extends BaseAdapter {
+
+
+    LayoutInflater inflater;
+    Context context;
+    List<StaticTextModel> items;
+
+    public StaticTextListAdapter(Context context, List<StaticTextModel> items) {
+        this.items = items;
+        this.context = context;
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
     @Override
     public int getCount() {
         return 0;
@@ -22,6 +50,16 @@ public class StaticTextListAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+
+        if (view == null) {
+            view = inflater.inflate(R.layout.activity_static_text_detail, viewGroup, false);
+            }
+
+        StaticTextModel a = items.get(i);
+
+        TextView tv = view.findViewById(R.id.textView3);
+        tv.setText(a.name);
+
+        return view;
     }
 }
--- app/src/main/java/ru/rubiconepro/study/Modules/StaticText/Layout/StaticTextList.java
+++ app/src/main/java/ru/rubiconepro/study/Modules/StaticText/Layout/StaticTextList.java
@@ -3,9 +3,14 @@ package ru.rubiconepro.study.Modules.StaticText.Layout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
+import ru.rubiconepro.study.Modules.StaticText.Model.StaticTextModel;
 import ru.rubiconepro.study.R;
 
 public class StaticTextList extends AppCompatActivity {
+
+    List<StaticTextModel> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

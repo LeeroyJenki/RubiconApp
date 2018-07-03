7c63d818dcbe0433acc82cee091d65805968b9e2
Grizzly <mixail_956@rambler.ru>
2018-07-03 21:01:50 +0300

    try to do dialog

--- app/src/main/java/ru/rubiconepro/study/Modules/Base/Helper/RequestHelper.java
+++ app/src/main/java/ru/rubiconepro/study/Modules/Base/Helper/RequestHelper.java
@@ -2,6 +2,12 @@ package ru.rubiconepro.study.Modules.Base.Helper;
 
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
+
+import okhttp3.Request;
+import ru.rubiconepro.study.Modules.Base.Interface.IDone;
+import ru.rubiconepro.study.Modules.PDFView.Layout.PDFViewLayout;
+import ru.rubiconepro.study.Modules.PDFView.PDFView;
 
 //Помошник по загрузке
 //1 - Составляет запрос
@@ -9,9 +15,34 @@ import android.content.Intent;
 //2 - Получает ответ
 //2.1 - завершает прилоадер
 //3 - в случае успеха запускает view
-public class RequestHelper {
+public class RequestHelper implements IDone {
     Context context;
     public RequestHelper(Context context, String url, Intent nextView) {
+        this.context = context;
 
+        Request r = null;
+        try {
+            r = new Request.Builder().url(url).build();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            r = null;
+        }
+        if (r == null) {
+            Toast.makeText(context, "Запрос неверен!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        PDFView.Current().loadData(r,
+                this);
+    }
+
+    @Override
+    public void JobDone(boolean success) {
+        if (!success) {
+            Toast.makeText(context, "Что то пошло не так!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Intent i = new Intent(context, nextView);
+        startActivity(i);
     }
 }
+
